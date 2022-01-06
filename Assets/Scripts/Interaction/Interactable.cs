78ed145ec9e2e05ee4ab542f5492b6829a99f565
poetahto <walls.dk@gmail.com>
2022-01-05 17:24:57 -0800

    improvements to movables

--- Assets/Scripts/Interaction/Interactable.cs
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -4,18 +4,25 @@ using UnityEngine.Events;
 public class Interactable : MonoBehaviour
 {
     [SerializeField] private float interactRange = 1.5f;
-    [SerializeField] private UnityEvent<GameObject, Vector3> onInteractStart;
-    [SerializeField] private UnityEvent onInteractEnd;
+    
+    [Space(20f)]
+    [SerializeField] public UnityEvent<GameObject, Vector3> onInteractStart;
+    [SerializeField] public UnityEvent onInteractEnd;
 
     public float InteractRange => interactRange;
+    public bool IsHeld { get; private set; }
+    public Vector3 GrabPoint { get; private set; }
     
     public void InteractStart(GameObject source, Vector3 point)
     {
+        GrabPoint = point;
+        IsHeld = true;
         onInteractStart.Invoke(source, point);
     }
 
     public void InteractEnd()
     {
+        IsHeld = false;
         onInteractEnd.Invoke();
     }
 }
