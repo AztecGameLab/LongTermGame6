aa4c4365be0f22aeeb3dccfb1d6fdb6095544800
poetahto <walls.dk@gmail.com>
2022-01-12 21:27:03 -0800

    crosshair fade anim

--- Assets/Scripts/CrosshairAnimator.cs
+++ Assets/Scripts/CrosshairAnimator.cs
@@ -3,18 +3,36 @@
 public class CrosshairAnimator : MonoBehaviour
 {
     [SerializeField] private InteractionSystem interactionSystem;
-    [SerializeField] private Transform crosshair;
+    [SerializeField] private CanvasGroup crosshair;
     [SerializeField] private Vector3 interactableScale;
     [SerializeField] private Vector3 normalScale;
     [SerializeField] private float animationSpeed;
+
+    [SerializeField] private float fadeSpeed = 1f;
     
     private void Update()
     {
-        Vector3 current = crosshair.localScale;
+        UpdateCrosshairAlpha();
+        UpdateCrosshairSize();
+    }
+
+    private void UpdateCrosshairAlpha()
+    {
+        float current = crosshair.alpha;
+        float target = interactionSystem.CurrentInteractable != null ? 0 : 1;
+
+        float maxDistanceDelta = fadeSpeed * Time.deltaTime;
+
+        crosshair.alpha = Mathf.MoveTowards(current, target, maxDistanceDelta);
+    }
+
+    private void UpdateCrosshairSize()
+    {
+        Vector3 current = crosshair.transform.localScale;
         Vector3 target = interactionSystem.LookingAtInteractable ? interactableScale : normalScale;
         
         float maxDistanceDelta = animationSpeed * Time.deltaTime;
         
-        crosshair.localScale = Vector3.MoveTowards(current, target, maxDistanceDelta);
+        crosshair.transform.localScale = Vector3.MoveTowards(current, target, maxDistanceDelta);
     }
 }
