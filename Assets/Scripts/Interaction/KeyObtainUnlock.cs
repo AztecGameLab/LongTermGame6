07035f70a1ede00297a086c1a4b98acb46265f57
EdenArts <ojedaeden08@gmail.com>
2022-01-31 23:09:31 -0800

    cloned the street level and made the rope animation

---
+++ Assets/Scripts/Interaction/KeyObtainUnlock.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyObtainUnlock : MonoBehaviour
+{
+    [Header("Dependencies")]
+    [SerializeField] public GameObject keyObject;
+    [SerializeField] public GameObject dummyLockPrefab;
+    [SerializeField] public GameObject animatedGameObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(keyObject))
+        {
+            animatedGameObject.GetComponent<Animator>().SetTrigger("Unlocked");
+            Instantiate(dummyLockPrefab, transform.position, transform.rotation);
+            Destroy(keyObject);
+            Destroy(gameObject);
+        }
+    }
+}
