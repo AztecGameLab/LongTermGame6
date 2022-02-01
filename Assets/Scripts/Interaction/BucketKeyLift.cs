fa728a0c8e02ab2f282055bd31d9919f43135cd5
EdenArts <ojedaeden08@gmail.com>
2022-02-01 12:46:42 -0800

    Finished the bucket lift animation

--- Assets/Scripts/Interaction/BucketKeyLift.cs
+++ Assets/Scripts/Interaction/BucketKeyLift.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyObtainUnlock : MonoBehaviour
+public class BucketKeyLift : MonoBehaviour
 {
     [Header("Dependencies")]
     [SerializeField] public GameObject keyObject;
-    [SerializeField] public GameObject dummyLockPrefab;
+    [SerializeField] public GameObject openedLockPrefab;
     [SerializeField] public GameObject animatedGameObject;
 
+
+    //Function makes the animation of the bucket, rope and key play
+    //Function also destroys the lock and key and spawns in the opened lock
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.Equals(keyObject))
         {
-            animatedGameObject.GetComponent<Animator>().SetTrigger("Unlocked");
-            Instantiate(dummyLockPrefab, transform.position, transform.rotation);
+            animatedGameObject.GetComponent<Animator>().SetTrigger("LiftBucket");
+            Instantiate(openedLockPrefab, transform.position, transform.rotation);
             Destroy(keyObject);
             Destroy(gameObject);
         }
