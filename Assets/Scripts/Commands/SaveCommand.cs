4f22acc4adda466d22007e10a960fba0879530af
Daniel Walls <walls.dk@gmail.com>
2022-02-04 13:04:17 -0800

    started save system

---
+++ Assets/Scripts/Commands/SaveCommand.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using ConsoleUtility;
+using Game;
+using UnityEngine.SceneManagement;
+
+namespace Commands
+{
+    [AutoRegisterConsoleCommand]
+    public class SaveCommand : IConsoleCommand
+    {
+        public string name => "save";
+        public string summary => "Interface with the save system.";
+        public string help => @"
+    * save create [saveName] : Create a new save named [saveName].
+    * save load [saveName] : Load a save named [saveName]";
+
+        public IEnumerable<Console.Alias> aliases { get; }
+        
+        public void Execute(string[] args)
+        {
+            if (args.Length > 1 && args[0] == "create")
+                HandleCreate(args[1]);
+
+            if (args.Length > 1 && args[0] == "load")
+                HandleLoad(args[1]);
+        }
+
+        private void HandleCreate(string saveName)
+        {
+            var newSave = new Save(saveName);
+            newSave.Write();
+        }
+
+        private void HandleLoad(string saveName)
+        {
+            var target = Save.Read(saveName);
+            SceneManager.LoadScene(target.CurrentScene);
+        }
+    }
+}
---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    [Serializable]
+    public class Save
+    {
+        public string CurrentScene { get; set; }
+        public string SaveName { get; private set; }
+        
+        public Save(string saveSaveName)
+        {
+            CurrentScene = SceneManager.GetActiveScene().name;
+            SaveName = saveSaveName;
+            
+            if (IsValid())
+                Debug.LogWarning($"There already exists a save \"{SaveName}\"! Be careful writing.");
+            
+            Debug.Log($"Created new save \"{saveSaveName}\".");
+        }
+
+        #region Instance Methods
+
+        public void Write()
+        {
+            Debug.Log($"Updated save {SaveName}.");
+            
+            // Creates a new file for the save.
+            string savePath = GetPath();
+            using FileStream saveFile = File.Create(savePath);
+            
+            // Writes our data to the file.
+            var binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(saveFile, this);
+        }
+        
+        public void Delete()
+        {
+            if (IsValid())
+            {
+                Debug.Log($"Deleted save {SaveName}.");
+                
+                string savePath = GetPath();
+                File.Delete(savePath);
+            }
+        }
+        
+        public bool IsValid()
+        {
+            return IsValid(SaveName);
+        }
+
+        public string GetPath()
+        {
+            return GetPath(SaveName);
+        }
+
+        #endregion
+        
+        #region Static Methods
+
+        public static Save Read(string saveName)
+        {
+            if (IsValid(saveName) == false)
+                throw new FileNotFoundException($"Tried to open the save \"{saveName}\" that doesn't exist!");
+        
+            Debug.Log($"Accessed the save \"{saveName}\".");
+            
+            // Opens the save file.
+            string savePath = GetPath(saveName);
+            using FileStream saveFile = File.OpenRead(savePath);
+            
+            // Parses the data from our file.
+            var binaryFormatter = new BinaryFormatter();
+            var saveData = (Save) binaryFormatter.Deserialize(saveFile);
+            return saveData;
+        }
+
+        public static bool IsValid(string saveName)
+        {
+            string savePath = GetPath(saveName);
+            return File.Exists(savePath);
+        }
+
+        public static string GetPath(string fileName)
+        {
+            return Application.persistentDataPath + $"/Saves/{fileName}.dat";
+        }
+
+        #endregion
+    }    
+}
