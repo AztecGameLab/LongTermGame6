38250cc8c0ff6ecb414f8ed20e3f080628cdb50e
EdenArts <ojedaeden08@gmail.com>
2022-02-06 13:41:24 -0800

    Update to the settings script and the settings scene
    currently there are options for resolution and graphics added

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -2,18 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer audioMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+    
+
+    Resolution[] resolutions;
+
+    private void Start()
     {
-        
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> resolutionOptions = new List<string>();
+
+        int currentResolutionsIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            resolutionOptions.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionsIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resolutionOptions);
+        resolutionDropdown.value = currentResolutionsIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetResolution(int resolutionIndex)
     {
-        
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
+    public void SetVolume(float volume)
+    {
+        //Updates the mixer with the slider value
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+
+
+    
 }
