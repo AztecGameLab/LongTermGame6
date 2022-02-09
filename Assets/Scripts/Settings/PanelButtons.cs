83be36e7503c125e4c11fb616a5fa8a768c593d2
EdenArts <ojedaeden08@gmail.com>
2022-02-08 23:39:43 -0800

    added some panels and the panel switching script

---
+++ Assets/Scripts/Settings/PanelButtons.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PanelButtons : MonoBehaviour
+{
+    public GameObject controlsPanel;
+    public GameObject audioPanel;
+    public GameObject graphicsPanel;
+
+    public GameObject gameobjectClosestToScreen;
+    public GameObject settingsCanvas;
+
+    public void BringControlPanelForward()
+    {
+        
+      // controlsPanel.transform.SetSiblingIndex()  //setttings canvas child count minus one
+
+    }
+    public void BringGraphicsPanelForward()
+    {
+
+    }
+    public void BringAudioPanelForward()
+    {
+
+    }
+}
