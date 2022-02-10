21c1377e4f98de7eabef25ea5e72fedad7871c73
EdenArts <ojedaeden08@gmail.com>
2022-02-10 13:44:08 -0800

    First attempt at making the control rebinding

---
+++ Assets/Scripts/Settings/Control_Rebinding.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_Rebinding : MonoBehaviour
+{
+    public void CustomizeController()
+    {
+
+        
+
+    }
+}
