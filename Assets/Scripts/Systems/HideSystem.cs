3c08ff8b096cfaf5923ec8892f33e4186f47914d
James Maron <jamesmaron87@gmail.com>
2022-02-10 20:27:05 -0800

    Hidesystem.cs

---
+++ Assets/Scripts/Systems/HideSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideSystem : MonoBehaviour
+{
+    public void hide()
+    {
+
+    }
+
+    public void unhide()
+    {
+
+    }
+}
