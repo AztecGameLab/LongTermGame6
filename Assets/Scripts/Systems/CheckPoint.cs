62f8e86a0f8ed850e432fcdb33e89c4ee3c1fe07
Tamarindo <idontknowwhattonamethiskhay@gmail.com>
2022-02-17 22:14:14 -0800

    i tried

--- Assets/Scripts/Systems/CheckPoint.cs
+++ Assets/Scripts/Systems/CheckPoint.cs
@@ -5,26 +5,20 @@ using UnityEngine;
 public class CheckPoint : MonoBehaviour
 {
     [SerializeField] private GameObject triggerObject;
-    [SerializeField] private List<GameObject> objectsToSave;
+    [SerializeField] private Game.GameObjectSaveData gameObjectSaveDataScript;
 
     public void Start()
     {
-        objectsToSave = new List<GameObject>();
+        
     }
     //private GameObject saveData = Game.SaveData.WriteData
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.Equals(triggerObject))
         {
-            objectsToSave.Add(this.gameObject);
-            objectsToSave.Add(GameObject.Find("TowerEnemy"));
-            objectsToSave.Add(GameObject.Find("ButcherEnemy"));
-            Save();
+            gameObjectSaveDataScript.WriteData();
         }
     }
-    private void Save()
-    {
-     
-    }
+
 
 }
--- Assets/Scripts/Systems/GameObjectSaveData.cs
+++ Assets/Scripts/Systems/GameObjectSaveData.cs
@@ -7,26 +7,39 @@ namespace Game
 {
     public class GameObjectSaveData : SaveData
     {
-        struct Data
+        public struct Data
         {
             // all of the important state (position, rotation, is enabled, ect.)
             public Vector3 position;
             public Quaternion rotation;
+            public Vector3 scale;
             public bool isObjectEnabled;
         }
 
         public override string GetID()
         {
             // create some kind of unique ID for this object, that we can use to look it up when we reload
+            string uniqueID = this.gameObject.name + "1";
+            
             return "placeholder unique ID";
         }
 
         public override object WriteData()
         {
             // generate the data that represents this object's current state.
-            return new Data { position = transform.position };
+            Data data = new Data
+            {
+                position = transform.position,
+                rotation = transform.rotation,
+                scale = transform.localScale,
+                isObjectEnabled = this.gameObject.activeInHierarchy
+            };
+           // _instances.Add(data.position);
+            return data;
+            
+            //_instances.Add(Data);
         }
-
+       
         public override void ReadData(object data)
         {
             // apply our saved data to our current object!
