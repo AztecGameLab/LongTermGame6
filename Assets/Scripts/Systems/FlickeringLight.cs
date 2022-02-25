5421198dedaf8e394b753c33c2e9d1597d6dd637
Daniel Walls <walls.dk@gmail.com>
2022-02-25 12:07:47 -0800

    improved editor compatability

--- Assets/Scripts/Systems/FlickeringLight.cs
+++ Assets/Scripts/Systems/FlickeringLight.cs
@@ -1,51 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FlickeringLight  : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("The light to apply the flickering to.")]
+    private Light target;
+    
     [SerializeField] 
     [Tooltip("Change light Flicker randomness")]
-    [Range(0.1f, 0.9f)]
-    private float _randomFactorFlicker = 0.5f;
+    private float randomFactorFlicker = 0.5f;
+    
     [SerializeField] 
     [Tooltip("Change light Color randomness")]
-    [Range(0.1f, 0.9f)]
-    private float _randomFactorColor = 0.5f;
+    private float randomFactorColor = 0.5f;
+
+    [SerializeField]
+    private float minIntensity = 0.1f;
     
-    private GradientColorKey[] colorKey;
-    private GradientAlphaKey[] alphaKey;
-    private Gradient _gradient;
+    [SerializeField]
+    private float maxIntensity = 0.5f;
+    
+    [SerializeField] 
+    private Gradient colorGradient;
     
     private void Update()
     {
-        Light myLight = GetComponent<Light>();
-        float noise = Mathf.PerlinNoise(Time.time * _randomFactorFlicker, 0.0f);
-        Debug.Log("Current Noise: " + noise);
-        myLight.intensity = (float) (1 - (0.5 * noise));
-        myLight.color = GetColor();
-
+        float noise = Mathf.PerlinNoise(Time.time * randomFactorFlicker, 0.0f);
+        target.color = GetColor();
+        target.intensity = minIntensity + (maxIntensity - minIntensity) * noise;
     }
 
     private Color GetColor()
     {
-        _gradient = new Gradient();
-        
-        // Populate the color keys at the relative time 0 and 1 (0 and 100%)
-        colorKey = new GradientColorKey[2];
-        colorKey[0].color = new Color(0.92f, 0.27f, 0.07f);
-        colorKey[0].time = 0.0f;
-        colorKey[1].color = new Color(0.69f, 0.18f, 0.05f);
-        colorKey[1].time = 1.0f;
-
-        // Populate the alpha  keys at relative time 0 and 1  (0 and 100%)
-        alphaKey = new GradientAlphaKey[2];
-        alphaKey[0].alpha = 1.0f;
-        alphaKey[0].time = 0.0f;
-        alphaKey[1].alpha = 0.0f;
-        alphaKey[1].time = 1.0f;
-        
-        _gradient.SetKeys(colorKey, alphaKey);
-        return _gradient.Evaluate((float) (1 - 0.5 * Mathf.PerlinNoise(Time.time * _randomFactorColor, 0.0f)));
+        return colorGradient.Evaluate(Mathf.PerlinNoise(Time.time * randomFactorColor, 0.0f));
     }
 }
