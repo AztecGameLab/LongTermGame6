70b5eb7a80364992d3432f86fe6c7e1e7dbe54bd
ajhel <62628887+ajhela@users.noreply.github.com>
2022-03-11 14:08:02 -0800

    added script

---
+++ Assets/Scripts/Interaction/UnlockKeyLock.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.events;
+
+public class UnlockKeyLock : MonoBehaviour
+{
+
+
+    // public GameObject keyObject;
+    // public GameObject keyObject2;
+    // public GameObject keyObject3;
+    // public UnityEvent OnUnlock;
+    // private static boolean isKeyUnlocked, isKey2Unlocked, isKey3Unlocked;
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.gameObject.Equals(keyObject))
+    //     {
+    //         // OnUnlock.Invoke();
+    //         isKeyUnlocked = true;
+    //     }
+    //     if (other.gameObject.Equals(keyObject2))
+    //     {
+    //         // OnUnlock.Invoke();
+    //         isKey2Unlocked = true;
+    //     }
+    //     if (other.gameObject.Equals(keyObject3))
+    //     {
+    //         // OnUnlock.Invoke();
+    //         isKey3Unlocked = true;
+    //     }
+
+    //     if (isKeyUnlocked && isKey2Unlocked && isKey3Unlocked)
+    //     {
+    //         OnUnlock.Invoke();
+    //     }
+    // }
+    
+    
+}
