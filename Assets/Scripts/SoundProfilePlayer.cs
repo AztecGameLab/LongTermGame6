47bf5a52f6ff696e3b609b1ae4532209faf0f4e7
Daniel Walls <walls.dk@gmail.com>
2022-04-27 18:05:12 -0700

    more work on landing sounds

--- Assets/Scripts/SoundProfilePlayer.cs
+++ Assets/Scripts/SoundProfilePlayer.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using FMOD;
+using FMOD.Studio;
 using FMODUnity;
 using NaughtyAttributes;
 using UnityEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 namespace Game
 {
@@ -11,6 +15,7 @@ namespace Game
         [SerializeField] private EntitySoundProfile profile;
         [SerializeField] private GroundCheck groundCheck;
         [SerializeField] private CrouchSystem crouchSystem;
+        [SerializeField] private Rigidbody targetRigidbody;
 
         [SerializeField] private List<TerrainLayerDefinition> definitions;
         
@@ -27,7 +32,8 @@ namespace Game
 
         private void Awake()
         {
-            _detector = new TerrainDetector(Terrain.activeTerrain);
+            if (Terrain.activeTerrain != null)
+                _detector = new TerrainDetector(Terrain.activeTerrain);
         }
 
         public void PlayStep()
@@ -71,11 +77,53 @@ namespace Game
             RuntimeManager.PlayOneShotAttached(path, gameObject);
         }
 
+        private Queue<float> _groundSpeeds = new Queue<float>();
+        
+        private void FixedUpdate()
+        {
+            _groundSpeeds.Enqueue(targetRigidbody.velocity.y);
+
+            if (_groundSpeeds.Count > 3)
+                _groundSpeeds.Dequeue();
+        }
+
+        [SerializeField] private float maxVelocity = 1;
+
         public void PlayLand()
         {
             string surfaceTag = GetTag();
             string path = profile.FindSurface(surfaceTag).landSound;
-            RuntimeManager.PlayOneShotAttached(path, gameObject);
+            
+            if (targetRigidbody != null)
+            {
+                var instance = RuntimeManager.CreateInstance(path);
+                float t = Mathf.Clamp01(Mathf.Abs(_groundSpeeds.Peek()) / maxVelocity);
+                if (t <= 0.1f)
+                    t = 0;
+                float volume = Mathf.Lerp(0, 1, t);
+                instance.setVolume(volume);
+                instance.set3DAttributes(new ATTRIBUTES_3D
+                {
+                    forward = new VECTOR{x = transform.forward.x, y = transform.forward.y, z = transform.forward.z},
+                    position = new VECTOR{x = transform.position.x, y = transform.position.y, z = transform.position.z},
+                    up = new VECTOR{x = transform.up.x, y = transform.up.y, z = transform.up.z},
+                    velocity = new VECTOR{x = targetRigidbody.velocity.x, y = targetRigidbody.velocity.y, z = targetRigidbody.velocity.z},
+                });
+                instance.start();
+                StartCoroutine(DisposeLand(instance));
+            }
+
+            else
+            {
+                RuntimeManager.PlayOneShotAttached(path, gameObject);
+            }
+        }
+
+        private static IEnumerator DisposeLand(EventInstance instance)
+        {
+            yield return new WaitForSeconds(1f);
+            instance.stop(STOP_MODE.ALLOWFADEOUT);
+            instance.release();
         }
     }
 }
